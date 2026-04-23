83b0f9ac12bde183de54e4d09775ed81fd6e7d84
peanut250 <209579534+peanut250@users.noreply.github.com>
2026-04-22 20:33:18 -0500

    Add icecast radio metadata

--- src/app/components/player/radio-info.tsx
+++ src/app/components/player/radio-info.tsx
@@ -1,10 +1,56 @@
 import { RadioIcon } from 'lucide-react'
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { MarqueeTitle } from "@/app/components/fullscreen/marquee-title.tsx"
 import { Radio } from '@/types/responses/radios'
+import IcecastMetadataStats from 'icecast-metadata-stats'
 
 export function RadioInfo({ radio }: { radio: Radio | undefined }) {
   const { t } = useTranslation()
+  const [radioMetadata , setRadioMetadata] = useState({title: null, artist: null})
+  const streamUrl = radio?.streamUrl
+
+  useEffect(() => {
+    if (!streamUrl) {
+      setRadioMetadata({title: null, artist: null})
+      return
+    }
+    let iceListener: IcecastMetadataStats
+
+    try {
+      iceListener = new IcecastMetadataStats(streamUrl, {
+        interval: 10,
+        onStats: (stats) => {
+          const streamTitle = stats.StreamTitle ? stats.StreamTitle : stats.icy?.StreamTitle
+          if (streamTitle) {
+            const i = streamTitle.indexOf(' - ')
+
+            if (i < 0) {
+              setRadioMetadata({title: streamTitle.trim(), artist: null})
+            } else {
+              const trackArtist = streamTitle.slice(0, i).trim()
+              const trackTitle = streamTitle.slice(i + 3).trim()
+              setRadioMetadata({title: trackTitle, artist: trackArtist})
+            }
+          } else {
+            setRadioMetadata({title: null, artist: null})
+          }
+        },
+        sources: ['icy'],
+      })
+
+      iceListener.start()
+    } catch {
+      setRadioMetadata({title: null, artist: null})
+    }
+
+    return () => {
+      if (iceListener) {
+        iceListener.stop()
+      }
+      setRadioMetadata({title: null, artist: null})
+    }
+  }, [streamUrl])
 
   return (
     <Fragment>
@@ -15,17 +61,25 @@ export function RadioInfo({ radio }: { radio: Radio | undefined }) {
           data-testid="radio-icon"
         />
       </div>
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col w-[66%] max-w-full justify-end text-left overflow-hidden">
         {radio ? (
           <Fragment>
-            <span className="text-sm font-medium" data-testid="radio-name">
-              {radio.name}
+            <MarqueeTitle gap="mr-6" >
+              <span className="text-sm font-medium" data-testid="radio-title">
+                { radioMetadata.title && radioMetadata.title !== "" ? radioMetadata.title : "—" }
+              </span>
+            </MarqueeTitle>
+            <span
+              className="text-xs font-light text-muted-foreground"
+              data-testid="radio-artist"
+            >
+              { radioMetadata.artist && radioMetadata.artist !== "" ? radioMetadata.artist : "" }
             </span>
             <span
               className="text-xs font-light text-muted-foreground"
-              data-testid="radio-label"
+              data-testid="radio-name"
             >
-              {t('radios.label')}
+              { radio.name }
             </span>
           </Fragment>
         ) : (
